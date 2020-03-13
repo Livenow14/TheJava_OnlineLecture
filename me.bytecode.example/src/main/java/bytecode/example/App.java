ced769529186926a4d76de192d7ec6f326d20b51
Livenow14 <xowjd41@naver.com>
2020-03-13 15:53:44 +0900

    jarcoco를 이용한 코드 커버리지 측정

--- me.bytecode.example/src/main/java/bytecode/example/App.java
+++ me.bytecode.example/src/main/java/bytecode/example/App.java
@@ -1,13 +1,13 @@
 package bytecode.example;
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        ClassLoader classLoader = App.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
     }
 }
