4e0fe9f291a61afb4390e6cd94a82c29cf1a8927
Livenow14 <xowjd41@naver.com>
2020-03-13 18:08:22 +0900

    javaagent 실습_jar을 통한 바이트 코드 조작 실습, Masulsa 클래스의 edit_configuration에서vm옵션에 -javaagent: jar경로(띄어쓰기는 로 감싸주기)

--- me.bytecode.example/src/main/java/bytecode/example/Moja/Masulsa.java
+++ me.bytecode.example/src/main/java/bytecode/example/Moja/Masulsa.java
@@ -2,7 +2,9 @@ package bytecode.example.Moja;
 
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,14 +15,14 @@ public class Masulsa {
 
     public static void main(String[] args) {
 
-        try {
+/*        try {
             new ByteBuddy().redefine(Moja.class)
                     .method(named("pullOut")).intercept(FixedValue.value("Rabbits!"))
                     .make().saveIn(new File("C:\\Users\\Tae Jeong\\Desktop\\대학관련자료\\개인공부\\더자바_코드를 조작하는 다양한 방법\\TheJava_OnlineLecture\\me.bytecode.example\\target\\classes\\"));        //classes 까지의 path를 넣는다.
         } catch (IOException e) {
             e.printStackTrace();
-        }
-       //         System.out.println(new Moja().pullOut());       //위와와 같이 실행하면안됨
+        }*/
+               System.out.println(new Moja().pullOut());       //위와와 같이 실행하면안됨
 
     }
 }
